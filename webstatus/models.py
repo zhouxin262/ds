b71b2a6856d49790c469315776cc665893d8fdbc
zhouxin <zhouin262@qq.com>
2012-10-18 17:08:15 +0800

    异常悲剧

--- webstatus/models.py
+++ webstatus/models.py
@@ -11,7 +11,7 @@ class WebStatus(models.Model):
     xsgzs = models.CharField(max_length=10, verbose_name=u"学生工作室", choices=STATUS_CHOICES, default=u"正常")
     zxcs = models.CharField(max_length=10, verbose_name=u"在线测试", choices=STATUS_CHOICES, default=u"正常")
     jhkt = models.CharField(max_length=10, verbose_name=u"交互课堂", choices=STATUS_CHOICES, default=u"正常")
-    memo = models.TextField(max_length=10, verbose_name=u"备注", blank=True, default="")
+    memo = models.TextField(max_length=10, verbose_name=u"异常备注", blank=True, default="")
     log_date = models.DateField(null=True, blank=True, verbose_name="日期", default="%d-%d-%d" % (datetime.today().year, datetime.today().month, datetime.today().day))
     log_time = models.CharField(max_length=10, verbose_name=u"时间", choices=TIME_CHOICES, default="08:00")
     dateline = models.DateTimeField(auto_now_add=True)
